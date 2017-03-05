f53b35689f5501e3587b2bc31758194f09abc671
Sin <346490349@qq.com>
2017-03-05 17:45:41 +0800

    加入创建数据库和表的各项配置。。

---
+++ app/src/main/java/sin/simpleweather/db/Province.java
@@ -0,0 +1,31 @@
+package sin.simpleweather.db;
+
+import java.util.StringTokenizer;
+
+/**
+ * Created by Sin on 2017/3/5.
+ */
+
+public class Province extends  DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public  int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return  provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
